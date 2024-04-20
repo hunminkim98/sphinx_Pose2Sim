b967c663ae67ae61b91129a5500af7e5bae405f9
HunMin Kim <gns5758@inha.edu>
2024-04-20 14:23:33 +0900

    New

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src/my_package'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Pose2Sim'
+copyright = '2024, David Pagnon'
+author = 'David Pagnon'
+release = '0.8.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx_markdown_checkbox',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
